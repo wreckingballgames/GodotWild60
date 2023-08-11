280479d7149b9c051224cfce66f3713177716df4
Raymond McDonald <contact@wreckingballpublishing.com>
2023-08-11 17:01:49 -0500

    Set up player scene, copy assets into project

--- prototypes/godot/LetUsFingTogetherPrototype/player/player.gd
+++ prototypes/godot/LetUsFingTogetherPrototype/player/player.gd
@@ -1,28 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var speed: float = 10000
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var movement_axis := get_movement_input()
+	velocity.y = movement_axis * speed * delta
+	
 	move_and_slide()
+
+
+func get_movement_input() -> float:
+	return Input.get_axis("move_up", "move_down")
