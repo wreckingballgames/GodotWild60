e291b659f0b09246324f820c53fe4e05ec4571c0
Raymond McDonald <contact@wreckingballpublishing.com>
2023-08-12 17:20:15 -0500

    Write pseudocode for handling player being grabbed

--- prototypes/godot/LetUsFingTogetherPrototype/player/player.gd
+++ prototypes/godot/LetUsFingTogetherPrototype/player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 
+var is_grabbed: bool = false
+
+
 @export var speed: float = 60
 @export var lives: int = 3
 @export var flick_force: float = 2500
@@ -20,10 +23,6 @@ extends CharacterBody2D
 @onready var middle_finger_sprite: Sprite2D = $FingerSprites/MiddleFingerSprite
 @onready var index_finger_sprite: Sprite2D = $FingerSprites/IndexFingerSprite
 @onready var thumb_sprite: Sprite2D = $FingerSprites/ThumbSprite
-
-
-func _ready() -> void:
-	pass
 
 
 func _physics_process(delta: float) -> void:	
@@ -103,3 +102,14 @@ func flick(body: Node2D) -> void:
 	var flick_vector: Vector2 = global_position.direction_to(body.global_position) * flick_force
 	body.apply_impulse(flick_vector)
 
+
+func shake_off() -> void:
+	pass
+	# Pseudocode
+	# If a body enters the GrabArea (check with a signal)
+	# 	If the body is an Enemy (check with collision layer)
+	# 		Toggle a flag to track if the Player is being grabbed
+	# Every frame, if the player is being grabbed
+	# 	Increase shake_off_meter slowly
+	# 	If the shake_off_meter is not 0, reduce it after every flick release
+	# 	If the shake_off_meter fills, kill the player and reset the shake_off_meter
