6c81cd3f048118947a2254bb8743ea2cce8479ab
Raymond McDonald <contact@wreckingballpublishing.com>
2023-08-13 18:12:31 -0500

    Implement pausing

--- prototypes/godot/LetUsFingTogetherPrototype2/world/world.gd
+++ prototypes/godot/LetUsFingTogetherPrototype2/world/world.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+var is_paused: bool = false
+
 # Parallax Members
 var parallax_scroll: float = 0
 @export var parallax_scroll_speed: float = 200
@@ -13,15 +15,26 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	pause()
 	parallax_scrolling(delta)
 
 
 func parallax_scrolling(delta: float) -> void:
-	# Scroll background
-	parallax_scroll += parallax_scroll_speed * delta
-	parallax_layer_1.motion_offset.y = parallax_scroll / 2
-	parallax_layer_2.motion_offset.y = parallax_scroll
+	if not is_paused:
+		# Scroll background
+		parallax_scroll += parallax_scroll_speed * delta
+		parallax_layer_1.motion_offset.y = parallax_scroll / 2
+		parallax_layer_2.motion_offset.y = parallax_scroll
 
 
 func _on_death_zone_body_entered(body: Node2D) -> void:
 	body.queue_free()
+
+
+func pause() -> void:
+	if is_paused and Input.is_action_just_pressed("pause"):
+		get_tree().paused = false
+		is_paused = false
+	elif not is_paused and Input.is_action_just_pressed("pause"):
+		get_tree().paused = true
+		is_paused = true
